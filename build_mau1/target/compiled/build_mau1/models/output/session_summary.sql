a6925ceabfa2817dc3e652209c6dc92bae40f312
meera2632 <93522113+meera2632@users.noreply.github.com>
2025-04-10 10:44:46 -0700

    Add files via upload

--- build_mau1/target/compiled/build_mau1/models/output/session_summary.sql
+++ build_mau1/target/compiled/build_mau1/models/output/session_summary.sql
@@ -1,1 +1,33 @@
+WITH  __dbt__cte__user_session_channel as (
+WITH user_session_channel_cte AS (
+    SELECT
+        userId,
+        sessionId,
+        channel
+    FROM dev.raw.user_session_channel
+)
 
+SELECT * FROM user_session_channel_cte
+),  __dbt__cte__session_timestamp as (
+WITH session_timestamp AS (
+    SELECT
+        sessionId,
+        ts
+    FROM dev.raw.session_timestamp
+)
+
+SELECT * FROM session_timestamp
+), u AS (
+    SELECT * FROM __dbt__cte__user_session_channel
+),
+st AS (
+    SELECT * FROM __dbt__cte__session_timestamp
+)
+SELECT 
+    u.userId, 
+    u.sessionId, 
+    u.channel, 
+    st.ts
+FROM u
+JOIN st 
+    ON u.sessionId = st.sessionId
